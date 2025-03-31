52d81d9cea4d212f77d2a088f4aa798c5262dcd6
Alex Krantz <alex@krantz.dev>
2025-03-30 17:00:21 -0700

    feat(http): add api gateway response generators

---
+++ internal/http/lambda/responses.go
@@ -0,0 +1,36 @@
+package lambda
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/akrantz01/tailfed/internal/api"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+// Success creates a successful HTTP response with some data
+func Success[T any](data *T) *events.APIGatewayProxyResponse {
+	return makeJsonResponse(&api.Response[T]{
+		Success: true,
+		Data:    data,
+	}, http.StatusOK)
+}
+
+// Error creates an error HTTP response with a message and status code
+func Error(message string, statusCode int) *events.APIGatewayProxyResponse {
+	return makeJsonResponse(&api.Response[struct{}]{
+		Success: false,
+		Error:   message,
+	}, statusCode)
+}
+
+func makeJsonResponse[T any](body T, statusCode int) *events.APIGatewayProxyResponse {
+	encoded, _ := json.Marshal(body)
+	return &events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(encoded),
+	}
+}
