008aa4a3574468bcab85cfb649839a37cc4e8428
Alex Krantz <alex@krantz.dev>
2025-04-12 16:28:56 -0700

    fix(terraform): ensure lambda function can write to log group

--- terraform/modules/lambda/iam.tf
+++ terraform/modules/lambda/iam.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "permissions_policy" {
     effect = "Allow"
     actions = [
       "logs:CreateLogStream",
-      "logs:PutLogEvent",
+      "logs:PutLogEvents",
     ]
     resources = ["${aws_cloudwatch_log_group.handler.arn}:*"]
   }
