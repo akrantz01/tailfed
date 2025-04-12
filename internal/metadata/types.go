a2757752bf849d41d7760716a6bf52ad0eb2b2d3
Alex Krantz <alex@krantz.dev>
2025-04-12 00:06:20 -0700

    feat(metadata): add minimal oidc discovery document

--- internal/metadata/types.go
+++ internal/metadata/types.go
@@ -1,6 +1,10 @@
 package metadata
 
-import "context"
+import (
+	"context"
+
+	"github.com/go-jose/go-jose/v4"
+)
 
 // Backend provides a mechanism for storing OpenID Connect metadata
 type Backend interface {
@@ -8,4 +12,29 @@ type Backend interface {
 	Load(ctx context.Context, key string, out any) error
 	// Save stores a new set of metadata. The metadata must be serializable to JSON
 	Save(ctx context.Context, key string, data any) error
+}
+
+// DiscoveryDocument is an OpenID Connect discovery document for an AWS IAM identity to read
+type DiscoveryDocument struct {
+	Issuer            string                    `json:"issuer"`
+	JwksUri           string                    `json:"jwks_uri"`
+	Claims            []string                  `json:"claims_supported"`
+	ResponseTypes     []string                  `json:"response_types_supported"`
+	SigningAlgorithms []jose.SignatureAlgorithm `json:"id_token_signing_alg_values_supported"`
+	SubjectTypes      []string                  `json:"subject-types_supported"`
+}
+
+// NewDiscoveryDocument creates a new OpenID Connect discovery document from an issuer URL
+func NewDiscoveryDocument(issuer string) DiscoveryDocument {
+	return DiscoveryDocument{
+		Issuer:        issuer,
+		JwksUri:       issuer + "/.well-known/jwks.json",
+		Claims:        []string{"aud", "iat", "iss", "sub"},
+		ResponseTypes: []string{"id_token"},
+		SigningAlgorithms: []jose.SignatureAlgorithm{
+			jose.RS256, jose.RS384, jose.RS512,
+			jose.ES256, jose.ES384, jose.ES256,
+		},
+		SubjectTypes: []string{"public"},
+	}
 }
