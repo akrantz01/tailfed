a572fc97e703406f2cc29dae15278d3ec0a9f221
Alex Krantz <alex@krantz.dev>
2025-04-18 15:32:23 -0700

    feat(metadata): include extra claims in metadata document

--- internal/metadata/types.go
+++ internal/metadata/types.go
@@ -27,9 +27,13 @@ type DiscoveryDocument struct {
 // NewDiscoveryDocument creates a new OpenID Connect discovery document from an issuer URL
 func NewDiscoveryDocument(issuer string) DiscoveryDocument {
 	return DiscoveryDocument{
-		Issuer:        issuer,
-		JwksUri:       issuer + "/.well-known/jwks.json",
-		Claims:        []string{"aud", "iat", "iss", "sub"},
+		Issuer:  issuer,
+		JwksUri: issuer + "/.well-known/jwks.json",
+		Claims: []string{
+			"aud", "iat", "iss", "sub", "exp", "nbf",
+			"tailnet", "dns_name", "machine_name", "host_name",
+			"os", "tags", "authorized", "external",
+		},
 		ResponseTypes: []string{"id_token"},
 		SigningAlgorithms: []jose.SignatureAlgorithm{
 			jose.RS256, jose.RS384, jose.RS512,
